e40bdbfc99f3e787923d8607a44a1c2cd4f2bc37
Jacquese <jacqwhitson@gmail.com>
2023-04-24 10:52:28 -0400

    Initial Commit

---
+++ Snacktacku;ar/Snacktacku_arApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snacktacku_arApp.swift
+//  Snacktacku;ar
+//
+//  Created by Jacquese Whitson  on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Snacktacku_arApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
